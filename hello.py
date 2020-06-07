98c3cb9599367f90a5e14e1cf3dccff92a9e22b7
Shivam <shivams14021999@gmail.com>
2020-06-07 15:49:59 +0530

    Add form

--- hello.py
+++ hello.py
@@ -1,20 +1,26 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask import render_template
-from flask_moment import Moment
-from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
-moment = Moment(app)
+app.config['SECRET_KEY'] = 'Shivam'
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time = datetime.utcnow())
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name = name)
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 @app.errorhandler(404)
 def page_not_found(e):
